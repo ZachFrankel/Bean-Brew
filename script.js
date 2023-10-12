31c44f99861ee956273bd1e8ce0d919b8e713d2b
23Ansell <23AnsellS689@collyers.ac.uk>
2023-10-12 14:48:34 +0100

    updates :)

---
+++ script.js
@@ -0,0 +1,12 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(n) {
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].style.display = 'block';
+}
+
+setInterval(() => {
+    showSlide(currentSlide + 1);
+}, 3000);
